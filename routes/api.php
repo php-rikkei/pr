8f9c4559629aeb40f1ca8fc92aac7bba533d10e8
Luong Van Tien <tienlv@rikkeisoft.com>
2017-09-15 11:07:15 +0700

    demo api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/department/{id}', 'Api\DepartmentController@show');
+Route::get('/department', 'Api\DepartmentController@index');
